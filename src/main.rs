7931a31add52ba3c033a3b0b396d9634ea64583e
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-30 10:11:50 -0800

    Refactor file processing logic and add command line argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 use std::fs;
 use std::fs::read_dir;
+use std::env;
 
 fn main() {
-    // let src_dir = "/media/pi/taz/Master_Master_Resize2";
-    // // loop throught src_dir collecting filenames into a vec
-    // let mut grand_vec = Vec::new();
-    // let mut count = 0;
-    // for entry in read_dir(src_dir).unwrap() {
-    //     let mut fn_vec = Vec::new();
-    //     count += 1;
-    //     if count < 101 {
-    //         let entry = entry.unwrap();
-    //         let path = entry.path();
-    //         let filename = path.file_name().unwrap().to_str().unwrap();
-    //         fn_vec.push(filename.to_string());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <src_dir> <out_dir>", args[0]);
+        std::process::exit(1);
+    }
 
-    //     } else {
-    //         count = 0;
-    //         grand_vec.push(fn_vec);
-    //     }
-
-    // }
-
-    // print!("{:?}", grand_vec);
-
-    let src_dir = "/media/pipi/USB01/Master_JPG/";
-    let out_dir = "/media/pipi/USB01/Master_Split";
-    // loop throught src_dir collecting filenames into a vec
-    // let mut fn_vec: Vec<String> = Vec::new();
+    let src_dir = &args[1];
+    let out_dir = &args[2];
     let mut dircount = 1;
     let mut count = 0;
+
     for entry in read_dir(src_dir).unwrap() {
         count += 1;
         if count < 101 {
@@ -40,10 +24,12 @@ fn main() {
             let fname = fn_split.last().unwrap().to_string();
             let in_str = format!("{}{}", src_dir, filename);
             let out_dir = format!("{}/{}", out_dir, dircount);
-            //create our_dir if it doesn't exist
+
+            // Create out_dir if it doesn't exist
             if !fs::metadata(&out_dir).is_ok() {
                 fs::create_dir(&out_dir).expect("Unable to create directory");
             }
+
             let out_str = format!("{}/{}", out_dir, fname);
             let res = format!("{} ->\n{}", in_str, out_str);
             print!("{}", res);
--- test.sh
+++ test.sh
@@ -6,3 +6,8 @@ cd /media/pipi/taz/JPG;
 ls -l | wc -l;
 cd /media/pipi/USB01/JPG;
 ls -l | wc -l;
+cd /home/pipi/splitintodirs;
+git pull;
+cargo run --release -- /media/pipi/0123-4567/JPG /media/pipi/0123-4567/Master_Split;
+cargo run --release -- /media/pipi/taz/JPG /media/pipi/taz/Master_Split;
+cargo run --release -- /media/pipi/USB01/JPG /media/pipi/USB01/Master_Split;
